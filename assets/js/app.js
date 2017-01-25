818eed02a366e68cc88cab06dc55f23b8b88d941
Gareth Johns <info@garethjohnsdesign.com>
2017-01-25 00:19:21 +0000

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,18 @@ $(document).ready(function() {
     transition: function(url){ window.location.href = url; }
   });
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
